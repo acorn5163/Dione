28685fc38333167e15e8f785f2c41a7089c5ef6c
acorn5163 <keigo6217@gmail.com>
2026-02-26 03:34:01 +0900

    added index on article_view

--- src/assets/component/Article_View/Index.tsx
+++ src/assets/component/Article_View/Index.tsx
@@ -1,13 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Index_Button from './Index_Button'
 
-type Props = {
+type Props_index = {
   chapters:Array<[string,string]>
 }
 
-function Index({chapters}:Props) {
+
+function Index(props:Props_index) {
+  console.log(props)
   return (
-    
+     <div className="card sticky-top">
+      <div className='card body'>
+        <p>目次</p>
+         {props.chapters.map(([id,content]) => 
+        (<Index_Button key = {id} id = {id} content = {content}/>))}
+      </div>
+    </div>
   )
 }
 export default Index
--- src/assets/page/Article/Article_View.tsx
+++ src/assets/page/Article/Article_View.tsx
@@ -2,36 +2,54 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
+import Index from '../../component/Article_View/Index'
 
 type Params = {
   id: string;
 };
 
 function Article_View() {
-    const { id } = useParams<Params>();
-    const [html, setHtml] = useState("");
+  const { id } = useParams<Params>();
+  const [html, setHtml] = useState("");
+  const [headers, setHeaders] = useState<Array<[string, string]>>([]);
 
-    useEffect(() => {
-        fetch(`/public/${id}.html`)
-        .then((res) => res.text())
-        .then((data) => {
-            setHtml(data);
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, "text/html");
+  useEffect(() => {
+    if (!id) return;
 
-            let headers:Array<[string,string]> = [];
-            let rawheaders = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
-            rawheaders.forEach((el) => {
-                headers.push([el.localName,el.textContent])
-            })
-            headers.map(([a,b]) => console.log(a,b)
-        )
+    fetch(`/${id}.html`)
+      .then((res) => res.text())
+      .then((data) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, "text/html");
+
+        const rawheaders = doc.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+        const newHeaders: Array<[string, string]> = [];
+
+        rawheaders.forEach((el) => {
+          const text = el.textContent ?? "";
+          const headerId = `${el.localName}_${text}`;
+          el.id = headerId;
+          newHeaders.push([headerId, text]);
         });
-    }, []);
+
+        setHeaders(newHeaders);
+        setHtml(doc.body.innerHTML);
+      });
+  }, [id]);
 
   return (
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-  );
+    <>
+    <div className='row'>
+      <div className='col-3'>
+        <Index chapters={headers} />
+      </div>
+      <div className='col-9'>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </div>
+    </>
 
+  );
 }
 export default Article_View
